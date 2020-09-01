ce1a9935693fd3e9d0ee3561684536d8634cbe24
Zac <a19860101@gmail.com>
2020-09-01 21:59:33 +0800

    20200901

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -15,10 +15,22 @@
             <input type="text" name="title" id="title" class="form-control" value="{{$post->title}}">
         </div>
         <div class="form-group">
+            @if($post->cover === 'no-pic.png')
+            <input type="file" name="cover" >
+            @else
+            <img src="/storage/images/{{$post->cover}}" class="w-25">
+            @endif
+        </div>
+        <div class="form-group">
             <label for="content">文章內容</label>
             <textarea name="content" id="content" cols="30" rows="10" class="form-control">{{$post->content}}</textarea>
         </div>
         <input type="submit" value="編輯文章" class="btn btn-primary">
         <input type="button" value="取消" class="btn btn-danger" onclick="history.back()">
     </form>
+   
+    <script src="https://cdn.ckeditor.com/4.14.1/full/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace('content');
+    </script>
 @endsection
